9b8d7d10a3054c8fe297e0368dc43a982d278fed
Piero Ranalli <pranalli.github@gmail.com>
2016-12-18 15:30:23 +0200

    avoid ftcalc errors by considering the case of zero-rotation separately

--- lib/Detection/Grid/Catalogue.pm
+++ lib/Detection/Grid/Catalogue.pm
@@ -27,17 +27,32 @@ sub cleancat {
     my $y2 = $self->cellymax;
 
 
-    my $cmds = <<FTOOLS;
+    # check for special case of zero-rotation
+    my $cmdcalc;
+    if ($r == 0) {
+	$cmdcalc = <<FTOOLS1;
  ftcalc $c   $tn1 ROT_RA  'RA*cos($r)-DEC*sin($r)' clobber=yes
  ftcalc $tn1 $tn2 ROT_DEC 'RA*sin($r)+DEC*cos($r)' clobber=yes
+FTOOLS1
+    } else {
+	$cmdcalc = <<FTOOLS2;
+ ftcalc $c   $tn1 ROT_RA  'RA' clobber=yes
+ ftcalc $tn1 $tn2 ROT_DEC 'DEC' clobber=yes
+FTOOLS2
+    }
+
+my $cmdselect = <<FTOOLS3;
  ftselect $tn2 $tn1 'ROT_RA>=$x1'   clobber=yes
  ftselect $tn1 $tn2 'ROT_RA<$x2'    clobber=yes
  ftselect $tn2 $tn1 'ROT_DEC>=$y1'  clobber=yes
  ftselect $tn1 $tn2 'ROT_DEC<$y2'   clobber=yes
  mv $tn2 ok_$c
-FTOOLS
+FTOOLS3
 
-    for my $c ( split('\n',$cmds) ) {
+    for my $c ( split('\n',$cmdcalc) ) {
+	$sas->call( $pf->env.$c );
+    }
+    for my $c ( split('\n',$cmdselect) ) {
 	$sas->call( $pf->env.$c );
     }
 }
